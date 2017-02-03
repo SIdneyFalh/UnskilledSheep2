20bb539f455196469c1d04eeb304411e6d34cc7c
SidneyFalh <sidney.falhun@etu.univ-nantes.fr>
2017-02-03 01:56:49 +0100

    On ajoute l'auteur

--- app/Views/documents/documents.php
+++ app/Views/documents/documents.php
@@ -13,7 +13,7 @@
 					</a>
   				</div>
   				<div class="panel-footer">
-  					<a href="<?= Secu::xss($leDoc->site);?>" class="link"><?= Secu::xss($leDoc->login); ?></a>
+  					Auteur: <a href="<?= Secu::xss($leDoc->site);?>" class="link"><?= Secu::xss($leDoc->login); ?></a>
   				</div>
 			</div>
 		<?php endforeach; ?>
