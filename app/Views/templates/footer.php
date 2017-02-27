6c13e8e9d0466104db037cd9a536bf2f4ef96030
SidneyFalh <sidney.falhun@etu.univ-nantes.fr>
2017-02-28 00:26:08 +0100

    Modification du Footer

--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -1,6 +1,6 @@
 	</div>
-	<div id="footer">Site créé par <a class="link" href="https://twitter.com/DevS_id">DevS_id</a> pour UnskilledSheeps © 2016<br/>
-			<a class="link" href="http://bugrecovery.fr">UnskilledSheeps</a>
+	<div id="footer">Site créé par <a class="link" href="https://twitter.com/DevS_id">DevS_id</a> pour BugRecovery © 2016-2017<br/>
+			<a class="link" href="http://bugrecovery.fr">BugRecovery</a>
 	</div>
 </body>
 </html>
